f460916f71336c0ae41d8cd7a5af7f77409620fa
bo0tzz <git@bo0tzz.me>
2025-11-20 21:33:56 +0100

    cigogne support

--- priv/migrations/20250825210434-add_m25_base_tables.sql
+++ priv/migrations/20250825210434-add_m25_base_tables.sql
@@ -1,3 +1,4 @@
+--- migration:up
 create schema if not exists m25;
 
 create table if not exists m25.job (
@@ -49,3 +50,9 @@ create table if not exists m25.version (
   version timestamptz not null primary key,
   created_at timestamptz not null default now()
 );
+--- migration:down
+drop table m25.version;
+drop table m25.job;
+drop schema m25;
+
+--- migration:end
